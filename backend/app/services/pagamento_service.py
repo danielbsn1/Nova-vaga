f5e9e28acaf1e05193553d683f17375f655dd1d7
danielbsn1 <danielbsneto9@gmail.com>
2026-02-17 14:19:11 -0300

    criação de Rota de pagamento

--- backend/app/services/pagamento_service.py
+++ backend/app/services/pagamento_service.py
@@ -3,19 +3,26 @@ from app.core.config import settings
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
 def create_checkout_session(plano_id: str, user_id: int):
     session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': plano_id,
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url='http://localhost:3000/success',
-        cancel_url='http://localhost:3000/cancel',
+        mode="payment",
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price": plano_id,
+                "quantity": 1,
+            }
+        ],
+        metadata={
+            "user_id": user_id,
+            "plano_id": plano_id,
+        },
+        success_url=settings.FRONT_SUCCESS_URL,
+        cancel_url=settings.FRONT_CANCEL_URL,
     )
-    
+
     return {
         "checkout_url": session.url,
-        "session_id": session.id
+        "session_id": session.id,
     }
