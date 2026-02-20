446a8804a1569716bc09537a832171412091c689
danielbsn1 <jb375835@icloud.com>
2026-02-20 09:39:39 -0300

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 import sys
 import os
 
-# Adicionar o diretório raiz ao PYTHONPATH
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from sqlalchemy import engine_from_config
@@ -10,25 +10,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+e.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 from app.core.database import Base
 from app.models import user, vaga, candidatura, freelancer, pagamento, notificacao
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
